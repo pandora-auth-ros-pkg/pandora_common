515a34f201a52b9b3b80ee135d90b418dcf32857
mujx <siderisk@auth.gr>
2015-05-02 18:22:29 +0300

    Fix indentation errors

--- state_manager/include/state_manager/state_server.h
+++ state_manager/include/state_manager/state_server.h
@@ -38,15 +38,15 @@ typedef actionlib::SimpleActionServer<state_manager_msgs::RobotModeAction> Robot
 
 
 struct stateNode {
-	std::string nodeName;
-	int currentState;
-	int previousState;
-	bool isInit;
+  std::string nodeName;
+  int currentState;
+  int previousState;
+  bool isInit;
 };
 
 class StateServer {
   private:
-		/**
+    /**
      * The ROS Node Handle.
      */
     ros::NodeHandle _nh;
@@ -54,62 +54,62 @@ class StateServer {
     /**
      * Publisher to the state topic.
      */
-		ros::Publisher _statePublisher;
+    ros::Publisher _statePublisher;
 
-		/**
+    /**
      * Subscriber to the state topic.
      */
-		ros::Subscriber _acknowledgeSubscriber;
+    ros::Subscriber _acknowledgeSubscriber;
 
-		/**
+    /**
      * Register service.
      */
-		ros::ServiceServer _registerService;
+    ros::ServiceServer _registerService;
 
-		/**
+    /**
      * State info service.
      */
-		ros::ServiceServer _stateInfoService;
+    ros::ServiceServer _stateInfoService;
 
-		/**
+    /**
      * Timer watchdog.
      */
-		ros::Timer _watchdog;
+    ros::Timer _watchdog;
 
-		/**
+    /**
      * Timer for monitoring.
      */
-		ros::Timer _timer;
+    ros::Timer _timer;
 
-		RobotModeActionServer _stateChangeActionServer;
+    RobotModeActionServer _stateChangeActionServer;
 
-		boost::mutex _mut;
-		boost::condition_variable _cond;
+    boost::mutex _mut;
+    boost::condition_variable _cond;
 
-		/**
+    /**
      * The diagnostic updater for posting on runtime_monitor.
      */
-		diagnostic_updater::Updater _updater;
+    diagnostic_updater::Updater _updater;
 
-		/**
+    /**
      * Variable indicating that the system is transitioning.
      */
-		bool _transitioning;
+    bool _transitioning;
 
-		/**
+    /**
      * Client State information getter (callback).
      */
-		void clientStateInformation(const state_manager_msgs::RobotModeMsgConstPtr&);
+    void clientStateInformation(const state_manager_msgs::RobotModeMsgConstPtr&);
 
-		/**
+    /**
      * Register node to a new state.
      */
-		void registerNodeTransition(const state_manager_msgs::RobotModeMsgConstPtr& msg);
+    void registerNodeTransition(const state_manager_msgs::RobotModeMsgConstPtr& msg);
 
-		/**
+    /**
      * Register a node.
      */
-		bool registerNode(state_manager_msgs::RegisterNodeSrv::Request& req,
+    bool registerNode(state_manager_msgs::RegisterNodeSrv::Request& req,
                       state_manager_msgs::RegisterNodeSrv::Response& res);
 
     /**
@@ -118,103 +118,103 @@ class StateServer {
     bool getStateInfo(state_manager_msgs::GetStateInfo::Request& req,
                       state_manager_msgs::GetStateInfo::Response& res);
 
-		/**
+    /**
      * Send a transition request to all nodes.
      */
-		void sendTransitionRequest(int newState);
+    void sendTransitionRequest(int newState);
 
-		/**
+    /**
      * Send the start msg to nodes.
      */
-		void sendStart(int newState);
+    void sendStart(int newState);
 
-		/**
+    /**
      * The current state.
      */
-		int _currentState;
+    int _currentState;
 
-		/**
+    /**
      * The previous state.
      */
-		int _previousState;
+    int _previousState;
 
-		/**
+    /**
      * The number of nodes that Ack'ed.
      */
-		unsigned int _numOfNodesAcked;
+    unsigned int _numOfNodesAcked;
 
-		/**
+    /**
      * A vector of the subscribed node names.
      */
-		std::vector<stateNode> nodeNames;
+    std::vector<stateNode> nodeNames;
 
-		/**
+    /**
      * The names of the states.
      */
-		std::string stateNames[NUM_OF_STATES];
+    std::string stateNames[NUM_OF_STATES];
 
-		/**
+    /**
      * Get the state the system is.
      */
-		inline std::string getStateName(int state);
+    inline std::string getStateName(int state);
 
-		/**
+    /**
      * Perform a census of all running nodes.
      */
-		void performNodeCensus();
+    void performNodeCensus();
 
-		/**
+    /**
      * Get index of node in nodeNames.
      * @param name the name of the node being searched.
      */
-		int nodeIndex(std::string name);
+    int nodeIndex(std::string name);
 
-		/**
+    /**
      * Checks if a node is listed.
      * @param name the name of the node being searched.
      */
-		bool nodeListed(std::string name);
+    bool nodeListed(std::string name);
 
-		/**
+    /**
      * Watchdog timer waiting for system transition.
      */
-		void watchdog(const ros::TimerEvent&);
+    void watchdog(const ros::TimerEvent&);
 
-		/**
+    /**
      * Giving info about nodes and current state.
      */
-		void statusNode();
+    void statusNode();
 
-		/**
+    /**
      * Setting the name of state.
      */
-		void setStateNames();
+    void setStateNames();
 
-		/**
+    /**
      * Monitors the behavior of state manager.
      */
-		void stateDiagnostic(diagnostic_updater::DiagnosticStatusWrapper&);
+    void stateDiagnostic(diagnostic_updater::DiagnosticStatusWrapper&);
 
-		/**
+    /**
      * Monitors the initialization of the nodes.
      */
-		void initDiagnostic(diagnostic_updater::DiagnosticStatusWrapper&);
+    void initDiagnostic(diagnostic_updater::DiagnosticStatusWrapper&);
 
-		/**
+    /**
      * Displays the diagnostic messages.
      */
-		void publishDiagnostics(const ros::TimerEvent&);
+    void publishDiagnostics(const ros::TimerEvent&);
 
-		void stateChangeExecuteCb(const state_manager_msgs::RobotModeGoalConstPtr &goal);
+    void stateChangeExecuteCb(const state_manager_msgs::RobotModeGoalConstPtr &goal);
 
-		bool systemTransitioned;
+    bool systemTransitioned;
 
   public:
 
-		/**
+    /**
      * Constructor.
      */
-		StateServer();
+    StateServer();
 
 };
 
